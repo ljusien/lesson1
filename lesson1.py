cd59660443e8a9887e267502c0eaf33d634ee26c
ljusien <ljusien777@gmail.com>
2024-11-04 20:20:05 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,16 @@
+name="1st program"
+print(name)
+print(9**0.5)
+print(3.0*5)
+name="2nd program"
+print(name)
+print(9.99>9.98,1000!=1000.1)
+name="3rd program"
+print(name)
+print(2*2+2)
+print(2*(2+2))
+print(6==8)
+name="4th program"
+print(name)
+print(123.456)
+print(int(float('123.456') * 10) % 10)
